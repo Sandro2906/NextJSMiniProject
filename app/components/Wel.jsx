2ab8c00d0bdd54c482f0c35d396ec215dbe7ca3b
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-11-22 16:13:45 +0100

    Create Wel.jsx

---
+++ app/components/Wel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Wel = () => {
+  return (
+    <div className='bg-purple-400 w-full flex flex-col justify-center items-center'>
+    <h1 className='text-5Xl text-white txtshdw'>WELCOME</h1>
+    <p className='text-xl text-white txtshdw'>I've created a website using Next.js, an API, and JSON. The main purpose of the site is to showcase my skills in that area of programming. I didn't focus much on the design aspect; my goal was to create a functional site. The aesthetics were not my primary concern.</p>
+    <h1 className='text-xl text-white txtshdw'>Website: https://dummyjson.com</h1>
+    <h1 className='text-xl text-white txtshdw'>JSON,API: https://dummyjson.com/products</h1>
+    </div>
+  )
+}
+
+export default Wel
