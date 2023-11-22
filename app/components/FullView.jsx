9b6a4d6bf329f8143a62d6f4d50550d389dd98b7
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-11-22 16:27:13 +0100

    Update FullView.jsx

--- app/components/FullView.jsx
+++ app/components/FullView.jsx
@@ -11,10 +11,8 @@ const FullView = async () => {
   return (
       <div>
                <div className='bg-purple-400 w-full flex flex-col justify-center items-center'>
-    <h1 className='text-5Xl text-white txtshdw'>WELCOME</h1>
-    <p className='text-xl text-white txtshdw'>I've created a website using Next.js, an API, and JSON. The main purpose of the site is to showcase my skills in that area of programming. I didn't focus much on the design aspect; my goal was to create a functional site. The aesthetics were not my primary concern.</p>
-    <h1 className='text-xl text-white txtshdw'>Website: https://dummyjson.com</h1>
-    <h1 className='text-xl text-white txtshdw'>JSON,API: https://dummyjson.com/products</h1>
+    <h1 className='text-5xl text-white txtshdw'>WELCOME</h1>
+    <p className='text-xl text-white txtshdw w-[700px] h-auto'>I've created a website using Next.js, an API, and JSON. The main purpose of the site is to showcase my skills in that area of programming. I didn't focus much on the design aspect; my goal was to create a functional site. The aesthetics were not my primary concern.</p>
     </div>
     <div className='grid lg:grid-cols-2 p-4 gap-3 grid-cols-1'>
     {
