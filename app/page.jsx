2c1fa29a628af71896844bb51c99ae146a0f9fca
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-11-22 16:07:14 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 import FullView from "./components/FullView";
-import Welcome from "./components/Welcome";
+
 
 export default function Home() {
 
   return (
     <div>
-      <Welcome />
+  
       <FullView />
     </div>
   )
